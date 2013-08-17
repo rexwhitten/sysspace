987338a885443af4c966ca3c4777443199f5f661
Rex <Rex.whitten@gmail.com>
2013-08-16 20:36:53 -0400

    updating bootlocal

--- iso/boot/opt/bootlocal.sh
+++ iso/boot/opt/bootlocal.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 # put other system startup commands here
+
+#load dependencies
+tce-load -wi openssh
+tce-load -wi git 
+tce-load -wi mono
+tce-load -wi nodejs
+
+#security 
+echo tc:!QAZxsw2| chpasswd
+
+#git is installed now to pull down other stuff if needed
