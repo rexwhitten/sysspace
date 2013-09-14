68e582e2d2220b7f5d0f34b83700a1a309a19d65
Rex <Rex.whitten@gmail.com>
2013-09-14 16:29:25 -0400

    adding sysspace installed os virtual

--- iso/boot/opt/bootlocal.sh
+++ iso/boot/opt/bootlocal.sh
@@ -6,8 +6,18 @@ tce-load -wi openssh
 tce-load -wi git 
 tce-load -wi mono
 tce-load -wi nodejs
+tce-load -wi linux-headers-3.0.3-tinycore
+tce-load -wi eglibc_base-devel
+tce-load -wi eglibc_conv
+tce-load -wi lua 
+tce-load -wi tcc 
+
+#build redis 
+cd /home/tc/
+git clone git://github.com/rexwhitten/redis.git
 
 #security 
 echo tc:!QAZxsw2| chpasswd
 
 #git is installed now to pull down other stuff if needed
+-- get application scripts 
--- iso/boot/opt/bootsync.sh
+++ iso/boot/opt/bootsync.sh
@@ -2,5 +2,5 @@
 # put other system startup commands here, the boot process will wait until they complete.
 # Use bootlocal.sh for system startup commands that can run in the background 
 # and therefore not slow down the boot process.
-/usr/bin/sethostname box
+/usr/bin/sethostname __sysyspace__
 /opt/bootlocal.sh &
